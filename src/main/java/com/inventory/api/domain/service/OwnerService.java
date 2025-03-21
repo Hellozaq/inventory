4f976cefa45ec6a854d5c295bea64ca0ae0e8f42
Miriã Fassarella <miriafassarella@gmail.com>
2025-03-20 22:19:46 -0400

    exception owner in use

--- src/main/java/com/inventory/api/domain/service/OwnerService.java
+++ src/main/java/com/inventory/api/domain/service/OwnerService.java
@@ -5,6 +5,7 @@ import com.inventory.api.domain.model.Person;
 import com.inventory.api.domain.model.Product;
 import com.inventory.api.domain.repository.OwnerRepository;
 import com.inventory.api.domain.repository.ProductRepository;
+import com.inventory.api.handler.OwnerExceptionInUse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,10 +27,10 @@ public class OwnerService {
     public void removeOwner(Long id) {
         Optional<Owner> owner = ownerRepository.findById(id);
         if (productRepository.existsByOwner(owner.get())) {
-            throw new RuntimeException("Não é possível excluir. Existem produtos associados a este tipo.");
-        }else{
-            ownerRepository.delete(owner.get());
+            throw new OwnerExceptionInUse("Il n'est pas possible de supprimer un propriétaire car il est lié à un ou plusieurs produits.");
         }
+            ownerRepository.delete(owner.get());
+
 
     }
 }
